92833e6ebaf55b6ca5c3f722644ca2922af1c472
EuanB <ejb9897@hotmail.co.uk>
2022-03-08 15:46:29 +0000

    Refactored methods to getAll

--- src/main/resources/static/CRUD.js
+++ src/main/resources/static/CRUD.js
@@ -22,52 +22,55 @@ function createGame(){  // Create value in db
     })
 }
 
-let refTable;
 function getGame() {
-    let refPromise = fetch('/getAll') // Get all
-    refPromise.then(
-        function (response) {
-            console.log(response.status)
-            if (response.status == 200) {
-                let refResponsePromise = response.json()
-                refResponsePromise.then(function (game) {
-                    console.log(game)
+    const table = document.getElementById("DataTable")
 
-                    refTable = document.createElement("table")
-                    refTable.style.border=1
-                    refTable.id = "DataTable";
-        
-                    let refRowHead= document.createElement("tr")
-                    let refDataHead1=document.createElement("td")
-                    let refDataHead2=document.createElement("td")
-                    let refDataHead3=document.createElement("td")
-                    let refDataHead4=document.createElement("td")
-                    let refDataHead5=document.createElement("td")
-
-                    refDataHead1.innerHTML="ID";
-                    refDataHead2.innerHTML="NAME";
-                    refDataHead3.innerHTML="GENRE";
-                    refDataHead4.innerHTML="HOURS PLAYED";
-                    refDataHead5.innerHTML="DELETE";
-
-                    refRowHead.appendChild(refDataHead1)
-                    refRowHead.appendChild(refDataHead2)
-                    refRowHead.appendChild(refDataHead3)
-                    refRowHead.appendChild(refDataHead4)
-                    refRowHead.appendChild(refDataHead5)
-
-                    refTable.appendChild(refRowHead)
-
-                    for (let i = 0; i < game.length; i++) {
-                        appendTable(game[i].id,game[i].name,game[i].genre, game[i].hoursPlayed);
-                    }
-                })
-            }
-            else {
-                alert("Server not found")
-            }
+    fetch('/getAll').then((response) => {
+        if (response.status == 200) {
+            return response.json();
+        } else {
+            alert("server not found")
         }
-    )
+    }).then((data) => {
+        console.log(data)
+        data.forEach(row => {
+            table.appendChild(createRow(row))
+        });
+    })
+}
+
+function createRow(rowData){
+    const row = document.createElement("tr")
+
+    row.id=`row-${rowData.id}`
+
+    const id = document.createElement("td")
+    id.innerHTML = rowData.id;
+    row.appendChild(id)
+
+    const name = document.createElement("td")
+    name.innerHTML = rowData.name;
+    row.appendChild(name)
+
+    console.log(rowData.genre)
+    const genre = document.createElement("td")
+    genre.innerHTML = rowData.genre;
+    row.appendChild(genre)
+
+    const hours = document.createElement("td")
+    hours.innerHTML = rowData.hoursPlayed;
+    row.appendChild(hours)
+
+    const actions = document.createElement("td")
+    const delBtn = document.createElement("input")
+    delBtn.type = "button"
+    delBtn.value = "Delete"
+    delBtn.addEventListener("click", () => deleteRow(rowData.id))
+    actions.appendChild(delBtn)
+    row.appendChild(actions)
+
+
+    return row;
 }
 
 function getGameByID() {
@@ -98,47 +101,11 @@ function getGameByID() {
     )
 }
 
-function appendTable(id, Gname, genre, hours){
-    let refRow1 = document.createElement("tr")
-
-    let refTd1 = document.createElement("td")
-    let refTd2 = document.createElement("td")
-    let refTd3 = document.createElement("td")
-    let refTd4 = document.createElement("td")
-    let refTd5 = document.createElement("td")
-
-    let refDelBtn = document.createElement("input")
-    let refUpdateBtn = document.createElement("input")
-
-	refDelBtn.type = "button"
-    refDelBtn.value = "delete"
-    refDelBtn.onclick = function() {deleteRow(this)};
-
-
-
-    refTd1.innerHTML = id
-    refTd2.innerHTML = Gname
-    refTd3.innerHTML = genre
-    refTd4.innerHTML = hours
-    refTd5.appendChild(refDelBtn);
-
-    refRow1.appendChild(refTd1)
-    refRow1.appendChild(refTd2)
-    refRow1.appendChild(refTd3)
-    refRow1.appendChild(refTd4)
-    refRow1.appendChild(refTd5)
-    
-
-    refTable.appendChild(refRow1)
-    document.body.append(refTable)
-}
-
-
-
-function deleteRow(element) {
-  var rowdel = element.parentNode.parentNode.rowIndex;
-  console.log(rowdel)
-  document.getElementById("DataTable").deleteRow(rowdel);
+function deleteRow(rowID) {
+    const row = document.getElementById(`row-${rowID}`)
+    row.remove()
+    deleteByID(rowID)
+    console.log(row)
 }
 
 
@@ -167,16 +134,19 @@ function update() {
     })
 }
 
-function deleteByID() {
-    let refPromise = fetch('/remove/'+gameId, {
-    method: 'DELETE',
+function deleteByID(gameId) {
+    let failed = false;
+    fetch(`/remove/${gameId}`, {
+        method: 'DELETE',
     })
-    .then(response => response.json())
-    .then(game => {
-        console.log('', game);
-        getGame();
-    })
-    .catch((error) => {
-        console.error('Error =', error);
-    })
+        .then(response => {
+            if (response == 204) {
+                console.log(`Received: ${response.status}`)
+            } else {
+                console.log(`Expected: 204, Actual: ${response.status}`)
+            }
+        })
+        .catch((error) => {
+            console.error('Error =', error);
+        })
 }
